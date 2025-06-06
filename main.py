4adf44e3c26938d64ede6c48d240e8e542ad633a
Parijaat Gaur <parijaat.gaur@outlook.com>
2025-06-07 00:51:43 +0530

    Update until fourth video -- funcitonal CLI chatbot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 import torch
+import random
 from tools import all_words
-from create_samples import create_data
+from create_samples import create_vector, create_data
 from train_file import train_fn
 
 f = open("all_intents_js.json")
@@ -25,9 +26,25 @@ model = train_fn(train, target, len(words_list), len(dt["data"]))
 # passing into the train_fn training and target data, and the dimensions of the vocab and no. of intents
 
 
+# print(len(dt["data"][pred_num]["responses"]))
+
+# chatting 
+while True:
+    inp = input("You: ")
+    vec = create_vector(inp, words_list)
+    input_vec = torch.as_tensor(vec, dtype=torch.float32)[None, ...]
+    output = model(input_vec)
+    pred_num = output.argmax().item()
+    resps = intents_response[pred_num]
+    # print("Bot: ", resps[random.randint(0, len(dt["data"][pred_num]["responses"]) - 1)])
+    print("Bot: ", resps[random.randint(0, 2)])
+
+
+
 
 
 # parsed and loaded the json file as a list into a variable dt
 # created vocabulary all_words (using fns in tools.py) from all the queries (cleaned) in dt
+# created numerical mappings of intents and responses
 # made train and target data using cleaning and vectorization
-# wrote script to define model, 
+# wrote script to define model in pytorch, and train it with prepared data
